19684973115e588d4214e988c6b70faac5e05e72
Akshay Narayan <okkhoy@gmail.com>
2018-08-24 12:53:22 +0800

    Add constructor

--- src/org/dessert/icecream/IceCream.java
+++ src/org/dessert/icecream/IceCream.java
@@ -6,6 +6,10 @@ public class IceCream {
     private String flavor;
     private int price;
 
+    IceCream(String color, String flavor) {
+        this.color = color;
+        this.flavor = flavor;
+    }
     public static void main(String args[]) {
 
     }
