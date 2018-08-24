89d7ba0716e5e4fd0d6f694e6faf76fef9a4e7c4
Akshay Narayan <okkhoy@gmail.com>
2018-08-24 12:52:06 +0800

    Add attributes

--- src/org/dessert/icecream/IceCream.java
+++ src/org/dessert/icecream/IceCream.java
@@ -2,6 +2,10 @@ package org.dessert.icecream;
 
 public class IceCream {
 
+    private String color;
+    private String flavor;
+    private int price;
+
     public static void main(String args[]) {
 
     }
